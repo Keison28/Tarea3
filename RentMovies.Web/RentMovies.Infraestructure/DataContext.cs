6eccab74337704f90eaaec9320d912f34ab06881
Keison28 <felucho.cerda@gmail.com>
2025-03-26 01:53:01 -0400

    Tarea 3 completa

---
+++ RentMovies.Web/RentMovies.Infraestructure/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RentMovies.Domain.Entities;
+
+namespace RentMovies.Ifraestructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RentMovies.Domain.Entities.Movie> Movies { get; set; } = default!;
+        public DbSet<RentMovies.Domain.Entities.Category> Categories { get; set; } = default!;
+
+    }
+}
